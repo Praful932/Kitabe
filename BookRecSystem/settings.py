070c8a4189012ae63deac1598000e2b0e0798357
Praful <praful.mohanan@gmail.com>
2020-09-03 20:42:15 +0530

    Add layout page

--- BookRecSystem/settings.py
+++ BookRecSystem/settings.py
@@ -127,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,  'static')
+]
 
 AUTHENICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
