e1ad6cc0700dded904e3e1f950d71a9d7884c690
Praful <praful.mohanan@gmail.com>
2020-09-08 19:05:55 +0530

    Add modal with book summary

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,4 +3,5 @@ from mainapp import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('search_ajax/', views.search_ajax, name='search_ajax'),
+    path('book_summary_ajax/', views.book_summary_ajax, name='summary_ajax')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,9 +3,11 @@ from django.http import JsonResponse
 from django.contrib.staticfiles.storage import staticfiles_storage
 import BookRecSystem.settings as settings
 
+from bs4 import BeautifulSoup
 import pandas as pd
 import os
 import json
+import requests
 
 def index(request):
     return render(request, 'mainapp/index.html')
@@ -24,5 +26,18 @@ def search_ajax(request):
         top5_result = df_book[df_book['original_title'].str.contains(query, regex=False, case = False)][:5]
         top5_result = json.dumps(top5_result.to_dict('records'))
         
-        return JsonResponse({'success' : True, 'top5_result' : top5_result}, status = 200, )
+        return JsonResponse({'success' : True, 'top5_result' : top5_result}, status = 200)
+
+def book_summary_ajax(request):
+    if request.method == 'POST' and request.is_ajax():
+        bookid = request.POST.get('bookid', None)
+        URL = 'https://www.goodreads.com/book/show/' + bookid
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        div_container = soup.find(id = 'description')
+        booksummary = ""
+        for spantag in div_container.find_all('span'):
+            booksummary = spantag.text
+            break
+        return JsonResponse({'success' : True, 'booksummary' : booksummary}, status = 200)
     
