9036fbe607ce103b1eb7421e9e397ef7e053d397
prajakta mane <prajakta916mane1@gmail.com>
2020-11-02 19:52:00 +0530

    reset modified

--- BookRecSystem/settings.py
+++ BookRecSystem/settings.py
@@ -161,7 +161,7 @@ LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 
 # simple mail transfer protocal
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
