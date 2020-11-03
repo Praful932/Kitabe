b01fe0fee10768beeb697ceaf31f7cac373e31d3
unknown <praful.mohanan@gmail.com>
2020-11-03 21:33:13 +0530

    Add Embedding Rec

--- mainapp/helpers.py
+++ mainapp/helpers.py
@@ -1,20 +1,36 @@
 import pandas as pd
 import numpy as np
 import os
-from scipy.spatial.distance import cdist
+import pickle
 import BookRecSystem.settings as settings
 
 # For Count Vectorizer
 book_path = os.path.join(settings.STATICFILES_DIRS[0] + '/mainapp/dataset/books.csv')
-cosin_sim_path = os.path.join(settings.STATICFILES_DIRS[0] + '/mainapp/model_files/cosine_rating_sim.npz')
-book_indices_path = os.path.join(settings.STATICFILES_DIRS[0] + '/mainapp/model_files/indices.pkl')
+cosin_sim_path = os.path.join(settings.STATICFILES_DIRS[0] + '/mainapp/model_files/tf-idf/cosine_rating_sim.npz')
+book_indices_path = os.path.join(settings.STATICFILES_DIRS[0] + '/mainapp/model_files/tf-idf/indices.pkl')
 
+# For Embedding
+book_id_map_path = os.path.join(settings.STATICFILES_DIRS[0] + '/mainapp/model_files/surprise/book_raw_to_inner_id.pickle')
+book_raw_map_path = os.path.join(settings.STATICFILES_DIRS[0] + '/mainapp/model_files/surprise/book_inner_id_to_raw.pickle')
+book_embed_path = os.path.join(settings.STATICFILES_DIRS[0] + '/mainapp/model_files/surprise/book_embedding.npy')
+sim_books_path = os.path.join(settings.STATICFILES_DIRS[0] + '/mainapp/model_files/surprise/sim_books.pickle')
+
+with open(book_id_map_path, 'rb') as handle:
+    book_raw_to_inner_id = pickle.load(handle)
+
+with open(book_raw_map_path, 'rb') as handle:
+    book_inner_id_to_raw = pickle.load(handle)
+book_embedding = np.load(book_embed_path)
+
+with open(sim_books_path, 'rb') as handle:
+    sim_books_dict = pickle.load(handle)
 
 
 cols = ['original_title', 'authors', 'average_rating', 'image_url', 'book_id']
 
 df_book = pd.read_csv(book_path)
-# df_book_cleaned = pd.read_csv(book_cleaned_path)
+total_books = df_book.shape[0]
+
 
 def get_book_title(bookid):
     '''
@@ -40,6 +56,22 @@ def get_rated_bookids(user_ratings):
         book_id = rating.bookrating
         already_rated.append(book_id)
     return already_rated
+
+
+def get_raw_id(book_id):
+    '''
+        Returns raw_id given book_id
+    '''
+    raw_id = df_book[df_book.book_id == book_id]['r_index'].values[0]
+    return raw_id
+
+
+def get_bookid(raw_id_list):
+    '''
+        Returns bookid list given rawid list
+    '''
+    bookid_list = list(df_book[df_book.r_index.isin(raw_id_list)]['book_id'].values)
+    return bookid_list
 
 
 def genre_wise(genre, percentile=0.85):
@@ -75,6 +107,30 @@ def count_vectorizer_recommendations(bookid):
     return bookid_list
 
 
+def embedding_recommendations(sorted_user_ratings):
+    '''
+        Returns recommended book ids based on embeddings
+    '''
+
+    best_user_books = []
+    similar_bookid_list = []
+
+    for i, rating in enumerate(sorted_user_ratings):
+        if rating.bookrating < 4 or i > 10:
+            break
+        else:
+            best_user_books.append(rating.bookid)
+
+    for book in best_user_books:
+        raw_id = get_raw_id(book)
+        top_sim_books = [book for book, similiarity in sim_books_dict[raw_id][:2]]
+        similar_bookid_list.extend(top_sim_books)
+
+    similar_bookid_list = get_bookid(similar_bookid_list)
+
+    return similar_bookid_list
+
+
 def get_book_dict(bookid_list):
     '''
         Returns book details based on provided bookids
@@ -83,16 +139,17 @@ def get_book_dict(bookid_list):
     return rec_books_dict
 
 
-def combine_ids(cv_bookids, svd_bookids, already_rated):
+def combine_ids(cv_bookids, embedding_bookids, already_rated):
     '''
         Returns best bookids combining both approaches
     '''
     cv_bookids = list(cv_bookids.difference(already_rated))
-    top_3_cv = set(cv_bookids[:3])
-    svd_bookids = svd_bookids.difference(already_rated)
-    svd_bookids = list(svd_bookids.difference(top_3_cv))
-    top_6_svd = list(svd_bookids[:6])
+    top_3_cv = set(cv_bookids[:2])
+    embedding_bookids = embedding_bookids.difference(already_rated)
+    embedding_bookids = list(embedding_bookids.difference(top_3_cv))
+    top_3_cv = list(top_3_cv)
+    top_6_embed = list(embedding_bookids[:7])
     print(top_3_cv)
-    print(top_6_svd)
-    best_bookids = list(top_3_cv) + top_6_svd
+    print(top_6_embed)
+    best_bookids = top_3_cv + top_6_embed
     return best_bookids
