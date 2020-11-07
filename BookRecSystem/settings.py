02f3a073826da0c1941d5c72a3b45af92360047a
unknown <praful.mohanan@gmail.com>
2020-11-07 16:05:47 +0530

    CI Configure 1

--- BookRecSystem/settings.py
+++ BookRecSystem/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'jfx=_rr-y-vn8#%7jx*ze2qx*n7&81f@pq6d8i_-grxl4ru_&&'
 SECRET_KEY = os.environ.get('KITABE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
