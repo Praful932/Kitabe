ba472127c21161e2d310f5a235cae1b7070cc6b5
unknown <praful.mohanan@gmail.com>
2020-11-07 17:44:21 +0530

    Configure CI 2

--- BookRecSystem/settings.py
+++ BookRecSystem/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -187,4 +186,6 @@ MESSAGE_TAGS = {
 }
 
 # Django Heroku Settings
-django_heroku.settings(locals())
+if '/app' in os.environ['HOME']:
+    import django_heroku
+    django_heroku.settings(locals())
