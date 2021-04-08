9b3b3815ba652b9f2a90c957c247c230e2f2ec7f
arpit456jain <111arpit1@gmail.com>
2021-04-08 12:04:38 +0530

    trying to pass the checks

--- mainapp/views.py
+++ mainapp/views.py
@@ -109,7 +109,3 @@ def handler404(request, *args, **argv):
     response.status_code = 404
     return response
 
-def handler500(request, *args, **argv):
-    response = render(request,'mainapp/404.html')
-    response.status_code = 500
-    return response
