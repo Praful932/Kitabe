25d0033ea8d39ca1d7a2446d1bfb0e304cc41df4
arpit456jain <111arpit1@gmail.com>
2021-04-12 00:19:38 +0530

    made the requested changes

--- BookRecSystem/settings.py
+++ BookRecSystem/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('KITABE_SECRET_KEY')
+SECRET_KEY = "a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['kitabe-app.herokuapp.com']
+ALLOWED_HOSTS = ['kitabe-app.herokuapp.com', "*"]
 
 
 # Application definition
