c8fe1927bc689cef5f9ce15e92a2d7010b26941d
Praful932 <praful.mohanan@gmail.com>
2021-04-13 22:46:34 +0530

    debug var update

--- BookRecSystem/settings.py
+++ BookRecSystem/settings.py
@@ -187,6 +187,6 @@ MESSAGE_TAGS = {
 
 # Django Heroku Settings
 if 'CHECK_HEROKU' in os.environ:
-    DEBUG = os.environ.get('KITABE_DEBUG')
+    DEBUG = eval(os.environ.get('KITABE_DEBUG'))
     import django_heroku
     django_heroku.settings(locals())
